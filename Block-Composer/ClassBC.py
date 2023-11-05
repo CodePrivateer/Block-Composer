b1dfed38992397d09393e58fa74aab3a8b5f7cbb
CodePrivateer <122541254+CodePrivateer@users.noreply.github.com>
2023-11-05 20:23:53 +0100

    Completing the KeyboardHandler Class and implement the class in Block_Composer.py Outsource the constants to a separate Const.py file

--- Block-Composer/ClassBC.py
+++ Block-Composer/ClassBC.py
@@ -1,27 +1,27 @@
 import pygame
+import Const
 
-class KeyboardHandler:
-    def __init__(self):
-        self.paused = False
-        self.quit_game = False
-        self.quit_yes = False
-        self.start = False
-        
-    def handle_event(self, event, blocks, game_field):
+class KeyboardHandler:       
+    def handle_event(self, event, blocks, game_field, state):
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_p:
-                self.paused = not self.paused
+                state['paused'] = not state['paused']
             if event.key == pygame.K_q:
-                self.quit_game = not self.quit_game
+                state['quit_game'] = not state['quit_game']
             if event.key == pygame.K_y:
-                if self.quit_game:
-                    self.quit_yes = True
-            if event.key == pygame.K_LEFT:
+                if state['quit_game']:
+                    state['quit_yes'] = True
+            if event.key == pygame.K_LEFT:  # Wenn die gedrückte Taste die linke Pfeiltaste ist
                 if blocks[-1].column > 0:  # Wenn der Block nicht am linken Rand ist
                     blocks[-1].column -= 1  # Bewegen Sie den Block eine Spalte nach links
                     if blocks[-1].collides(game_field):  # überprüfen Sie, ob der Block mit dem Spielfeld kollidiert
                         blocks[-1].column += 1  # Wenn ja, bewegen Sie den Block eine Spalte nach rechts zurück
-            elif event.key == pygame.K_RIGHT:
-                # Fügen Sie hier den Code für die rechte Pfeiltaste hinzu
-                pass
-    pass
+            elif event.key == pygame.K_RIGHT:  # Wenn die gedrückte Taste die rechte Pfeiltaste ist
+                if blocks[-1].column < Const.SCREEN_WIDTH / Const.BLOCK_SIZE - len(blocks[-1].shape[0]):  # Wenn der Block nicht am rechten Rand ist
+                    blocks[-1].column += 1  # Bewegen Sie den Block eine Spalte nach rechts
+                    if blocks[-1].collides(game_field):  # überprüfen Sie, ob der Block mit dem Spielfeld kollidiert
+                        blocks[-1].column -= 1  # Wenn ja, bewegen Sie den Block eine Spalte nach links zurück
+            elif event.key == pygame.K_SPACE:  # Wenn die gedrückte Taste die Leertaste ist
+                blocks[-1].rotate(game_field)  # Drehen Sie den Block und übergeben Sie das Spielfeld an die rotate-Methode zur Kollisionsprüfung
+            elif event.key == pygame.K_DOWN:  # Wenn die gedrückte Taste die Pfeiltaste nach unten ist
+                state['speed'] = 10 * state['level']  # Erhöhen Sie die Geschwindigkeit, mit der der Block fällt
---
+++ Block-Composer/Const.py
@@ -0,0 +1,10 @@
+#Game Constants / Spielkonfiguration
+SCREEN_WIDTH, SCREEN_HEIGHT = 400, 600
+BLOCK_SIZE = 20
+POINTS_BLOCK = 5
+POINTS_ROW = 20
+LEVEL_POINTS = 250
+LEVEL_LINES = 4
+LEVEL_MAX = 5
+# Erstellen Sie einen zusätzlichen Bereich für die Anzeige von Punkten und Highscore
+SCORE_AREA_WIDTH = 250
