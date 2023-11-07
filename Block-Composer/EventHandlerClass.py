579d5968cee56c87d5a9871880565eac4388e6bb
CodePrivateer <122541254+CodePrivateer@users.noreply.github.com>
2023-11-07 21:05:12 +0100

    Implement block as instance

--- Block-Composer/EventHandlerClass.py
+++ Block-Composer/EventHandlerClass.py
@@ -4,7 +4,8 @@ import webbrowser
 import time
 
 class EventHandler:       
-    def handle_keyboard_event(self, events, blocks, game_field, state):
+
+    def handle_keyboard_event(self, events, block, game_field, state):
         for event in events:  # Durchlaufen Sie alle aufgetretenen Ereignisse
             if event.type == pygame.QUIT:  # Wenn das Ereignis QUIT ist (z.B. Schließen des Fensters)
                 pygame.quit()  # Beenden Sie die Funktion
@@ -17,20 +18,20 @@ class EventHandler:
                     if state['quit_game']:
                         state['quit_yes'] = True
                 if event.key == pygame.K_LEFT:  # Wenn die gedrückte Taste die linke Pfeiltaste ist
-                    if blocks[-1].column > 0:  # Wenn der Block nicht am linken Rand ist
-                        blocks[-1].column -= 1  # Bewegen Sie den Block eine Spalte nach links
-                        if blocks[-1].collides(game_field):  # überprüfen Sie, ob der Block mit dem Spielfeld kollidiert
-                            blocks[-1].column += 1  # Wenn ja, bewegen Sie den Block eine Spalte nach rechts zurück
+                    if block.column > 0:  # Wenn der Block nicht am linken Rand ist
+                        block.column -= 1  # Bewegen Sie den Block eine Spalte nach links
+                        if block.collides(game_field):  # überprüfen Sie, ob der Block mit dem Spielfeld kollidiert
+                            block.column += 1  # Wenn ja, bewegen Sie den Block eine Spalte nach rechts zurück
                 elif event.key == pygame.K_RIGHT:  # Wenn die gedrückte Taste die rechte Pfeiltaste ist
-                    if blocks[-1].column < Const.SCREEN_WIDTH / Const.BLOCK_SIZE - len(blocks[-1].shape[0]):  # Wenn der Block nicht am rechten Rand ist
-                        blocks[-1].column += 1  # Bewegen Sie den Block eine Spalte nach rechts
-                        if blocks[-1].collides(game_field):  # überprüfen Sie, ob der Block mit dem Spielfeld kollidiert
-                            blocks[-1].column -= 1  # Wenn ja, bewegen Sie den Block eine Spalte nach links zurück
+                    if block.column < Const.SCREEN_WIDTH / Const.BLOCK_SIZE - len(block.shape[0]):  # Wenn der Block nicht am rechten Rand ist
+                        block.column += 1  # Bewegen Sie den Block eine Spalte nach rechts
+                        if block.collides(game_field):  # überprüfen Sie, ob der Block mit dem Spielfeld kollidiert
+                            block.column -= 1  # Wenn ja, bewegen Sie den Block eine Spalte nach links zurück
                 elif event.key == pygame.K_SPACE:  # Wenn die gedrückte Taste die Leertaste ist
-                    blocks[-1].rotate(game_field)  # Drehen Sie den Block und übergeben Sie das Spielfeld an die rotate-Methode zur Kollisionsprüfung
+                    block.rotate(game_field)  # Drehen Sie den Block und übergeben Sie das Spielfeld an die rotate-Methode zur Kollisionsprüfung
                 elif event.key == pygame.K_DOWN:  # Wenn die gedrückte Taste die Pfeiltaste nach unten ist
                     state['speed'] = 10 * state['level']  # Erhöhen Sie die Geschwindigkeit, mit der der Block fällt
-                    
+
     def handle_mouse_event(self, events, link_rect):
         for event in events:  # Durchlaufen Sie alle aufgetretenen Ereignisse
             if event.type == pygame.QUIT:  # Wenn das Ereignis QUIT ist (z.B. Schließen des Fensters)
@@ -52,7 +53,7 @@ class EventHandler:
             if state['start']:
                 break
 
-    def handle_gameover_event(self, state, blocks, new_block, link_rect):
+    def handle_gameover_event(self, state, link_rect):
         state['restart_game'] = False
         state['start_screen_timer'] = time.time()  # Startzeit speichern
         while True:
@@ -61,9 +62,7 @@ class EventHandler:
             for event in events:
                 if event.type == pygame.QUIT:
                     return  
-                elif event.type == pygame.KEYDOWN:
-                    if not state['quit_yes']:
-                        blocks.append(new_block)  # Fügen Sie den neuen Block zur Liste hinzu und starten Sie das Spiel neu
+                elif event.type == pygame.KEYDOWN:                   
                     state['restart_game'] = True  
                     return
             self.handle_mouse_event(events, link_rect)
